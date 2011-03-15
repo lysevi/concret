66a813473549e19b8f04c6d314a07138a7d04ab9
IrishWithAxeWork <irishwithaxe@gmail.com>
2011-03-15 08:38:24 +0300

    Убрал LaTeX обвязку.

--- results/draw_errors.py
+++ results/draw_errors.py
@@ -18,12 +18,12 @@ if len(sys.argv)==1:
     print "usage: ",sys.argv[0]," filename"
     sys.exit()
 
-from matplotlib import rc
-rc('font',**{'family':'serif'})
-rc('text', usetex=True)
-rc('text.latex',unicode=True)
-rc('text.latex',preamble='\usepackage[utf8]{inputenc}')
-rc('text.latex',preamble='\usepackage[russian]{babel}')
+#from matplotlib import rc
+#rc('font',**{'family':'serif'})
+#rc('text', usetex=True)
+#rc('text.latex',unicode=True)
+#rc('text.latex',preamble='\usepackage[utf8]{inputenc}')
+#rc('text.latex',preamble='\usepackage[russian]{babel}')
 
 colors="g r w c m y k".split()
 
